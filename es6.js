db985ab462cacc37b48611b7ce0d36fe514b54ea
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-21 02:17:23 +0600

    first commit

---
+++ es6.js
@@ -0,0 +1,8 @@
+const numbers = [89, 35, 98, 12];
+const student = {
+    name : "Alexa",
+    age : 11,
+    movies : ['Amazon', 'Meril', 'Hit']
+}
+const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[0]}`;
+console.log(about);
